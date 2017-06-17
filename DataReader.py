2cb8e824d0ae9a8f973616e9339e8800d8a7e85c
aashish <aashish.venkatesh@student.uva.nl>
2017-06-17 11:00:20 +0200

    Image Features extracted

--- DataReader.py
+++ DataReader.py
@@ -134,7 +134,7 @@ class DataReader():
 
 
 
-dr = DataReader(data_path='preprocessed_new.h5', indicies_path='indices_new.json', images_path='val2014')
+# dr = DataReader(data_path='preprocessed_new.h5', indicies_path='indices_new.json', images_path='val2014')
 
 """ Example of how to use this class
 #game_ids = dr.get_game_ids()
