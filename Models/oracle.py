1a7c9fa634690dd1d23051d5388de60954cef3b3
Sanne Bouwmeester <sanne.wopwop@gmail.com>
2017-06-21 17:05:27 +0200

    Updated Oracle and train_oracle

--- Models/oracle.py
+++ Models/oracle.py
@@ -19,17 +19,17 @@ class Oracle(nn.Module()):
     # d_hin/d_hidden/d_hout: dimenties van hidden layer: 
     # --- helft van de dimensies die het verbind, recursively, voor gradual overgang.
     # d_out: 3 (Yes,No,N/A)
-    def __init__(self, embedding_dim,hidden_dim, d_in, d_hin, d_hidden, d_hout, d_out)
+    def __init__(self,vocab_size, embedding_dim, hidden_dim, d_in, d_hin, d_hidden, d_hout, d_out)
         # Dit weet ik allemaal niet zo goed meer: is dit nodig?
         super(Oracle, self).__init__()
         self.hidden_dim = hidden_dim
+        self.vocab_size = vocab_size
 
         # Word Embeddings
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
 
         # LSTM model that encodes Question
         self.lstm = nn.LSTM(embedding_dim,hidden_dim) 
-        self.hidden = self.init_hidden 
 
         # MLP model that classifies to an Answer
         self.mlp = nn.Sequential(
@@ -54,27 +54,21 @@ class Oracle(nn.Module()):
                     autograd.Variable(torch.zeros(1, 1, self.hidden_dim)))
 
     def forward(self, question, spatial_info, object_class, crop, image, training=True):
-        # Compute the one hot representation of the sentence
+        # Compute representation of the sentence
         sentence_embedding = Variable(torch.zeros(len(question.split()), self.word_embedding_dim))
-
         for i, w in enumerate(sentence.split()):
-            if w == '-SOS-':
-                sentence_embedding[i] = self.sos
-            else:
-                sentence_embedding[i] = self.word2embedd(w)
-
-        # Get tensor with word embeddings
-        encoder_in = self.word_embed_model(sent_onehot)
+            sentence_embedding[i] = self.word2embedd(w)
+        encoder_in = sentence_embedding.view(1,1,-1)
         
         # LSTM pass
-        encoder_out, self.hidden = self.lstm(encoder_in, self.hidden)
+        self.hidden = self.init_hidden 
+        encoder_out = self.lstm(encoder_in, self.hidden)
 
         #Answer question
         mlp_in = torch.cat([encoder_out, image, crop, spatial_info, object_class])
 
         # MLP pass
         mlp_out = self.mlp(mlp_in) 
-        #TODO: turn mlp_out into "Yes", "No", "N/A" softmax dus.
         return mlp_out 
 
 
--- train_Oracle.py
+++ train_Oracle.py
@@ -3,15 +3,33 @@
 from Preprocessing.DataReader import DataReader
 from Models.oracle import oracle.py
 
+def img_spatial(img_meta):
+    bbox            = img_meta[0]
+    width           = img_meta[1]
+    height          = img_meta[2]
+    image_center_x  = width / 2
+    image_center_y  = height / 2
+
+    x_min = (min(bbox[0], bbox[2]) - image_center_x) / image_center_x
+    y_min = (min(bbox[1], bbox[3]) - image_center_y) / image_center_y
+    x_max = (max(bbox[0], bbox[2]) - image_center_x) / image_center_x
+    y_max = (max(bbox[1], bbox[3]) - image_center_y) / image_center_y
+    x_center = (x_min + x_max) / 2
+    y_center = (y_min + y_max) / 2
+
+    w_box = x_max - x_min
+    h_box = y_max - y_min
+    
+return Variable(torch.FloatTensor([x_min, y_min, x_max, y_max, x_center, y_center, w_box, h_box]))
 
 def train():
     # Load data
     dr = DataReader()
 
     visual_len = 4096
-    object_len = 4096 #TODO: check if true
+    object_len = 4096 
     category_len = 1
-    spatial_len = 4
+    spatial_len = 8
     embedding_dim = 128
     word2index = dr.get_word2ind()
     vocab_size = len(word2index)
@@ -29,22 +47,26 @@ def train():
     #Instance of Oracle om LSTM en MLP te runnen?
     model = Oracle(embedding_dim,hidden_dim, d_in, d_hin, d_hidden, d_hout, d_out)
     loss = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters())
+    optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
     #Get Game/Question and run model
     gameids = dr.get_game_ids()
     for gid in gameids:
         image = dr.get_image_features(gid)
-        #crop = dr.get_crop_features(gid) #TODOL get features of cropped images
-        obj = get_target_object(gid)
+        crop = dr.get_crop_features(gid) 
+        obj_id = dr.get_target_object(gid)
+        object_class = get_category_id(gid)[obj_id] 
+        img_meta = dr.get_image_meta(gid) #TODO: uitzoeken waarvan de gereturnde bounding box is?
+        spatial = img_spatial(img_meta) #[obj_id] TODO: is dat nodig?
         quas = dr.get_questions(gid)
         answers = dr.get_answers(gid)
         for qi,question in quas:
             outputs = model.forward(question, spatial_info, object_class, crop, image)
-            answer = answers(qui)
+            answer = answers[qi]
             cost = loss(outputs,answer)
+            print(cost.data[0])
 
-            # Backpropogate Errors ||TODO: also applies to LSTM?
+            # Backpropogate Errors 
             optimizer.zero_grad() 
             cost.backward()
             optimizer.step()
