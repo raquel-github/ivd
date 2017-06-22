11c35aa98afb4722e6a9699372feeb7249ed66b2
aashish <aashish.venkatesh@student.uva.nl>
2017-06-22 19:01:13 +0200

    Some progress in batching

--- batchutil.py
+++ batchutil.py
@@ -64,12 +64,96 @@ def create_batch_matrix(batch, dr, word2index, pad_token):
         for _ in range(max_dec_length-len(db)):
             decoder_batch_list[i].append(pad_token)
 
-    encoder_batch_matrix = encoder_batch_list
-    decoder_batch_matrix = decoder_batch_list
+    encoder_batch_matrix = np.asarray(encoder_batch_list)
+    decoder_batch_matrix = np.asarray(decoder_batch_list)
 
-    print(type(encoder_batch_matrix))
-    print(type(decoder_batch_matrix))
+    print(word2index['-SOS-'])
+    print(word2index['-PAD-'])
+    print(encoder_batch_matrix)
+    print(decoder_batch_matrix)
     return encoder_batch_matrix, decoder_batch_matrix
+
+def create_batch_matrix(batch, dr, word2index, pad_token):
+    """ given a batch, this function will create the input to the encoder for every game in the batch
+    and the target for the decoder for every game in the batch """
+    encoder_batch_list = [[] for i in range(len(batch))]
+    decoder_batch_list = [[] for i in range(len(batch))]
+
+    max_questions = 0
+
+    for i, gid in enumerate(batch):
+        questions           = dr.get_questions(gid)
+        answers             = dr.get_answers(gid)
+
+        if max_questions < len(questions):
+            max_questions = len(questions)
+
+        # get the entire dialoge
+        game_dialogue       = str()
+        question_dialogue   = str()
+        for q, a in zip(questions, answers):
+            game_dialogue = q + ' ' + a + ' '
+            question_dialogue = q + ' '
+            # convert the dialoge to indices
+            game_dialogue_ids       = list()
+            for w in game_dialogue.split():
+                game_dialogue_ids.append(word2index[w])
+            question_dialogue_ids   = list()
+            for w in question_dialogue.split():
+                question_dialogue_ids.append(word2index[w])
+            encoder_batch_list[i].append(game_dialogue_ids)
+            decoder_batch_list[i].append(question_dialogue_ids)
+
+    print('max ques', max_questions)
+    # print('encoder: ',encoder_batch_list)
+    # print('decoder: ',decoder_batch_list)
+
+    encoder_questions_list = [[] for i in range(max_questions)]
+    encoder_questions_maxlength = [[0] for i in range(max_questions)]
+
+    for i in range(max_questions):
+        for j in range(len(batch)):
+            if i < len(encoder_batch_list[j]):
+                encoder_questions_list[i].append(encoder_batch_list[j][i])
+                if encoder_questions_maxlength[i][0] < len(encoder_batch_list[j][i]):
+                    encoder_questions_maxlength[i][0] = len(encoder_batch_list[j][i])
+            else:
+                encoder_questions_list[i].append([])
+
+    print(encoder_questions_list)
+    print(encoder_questions_maxlength)
+
+
+    max_enc_length = 0
+    max_dec_length = 0
+
+    for eb in encoder_batch_list:
+        if len(eb) > max_enc_length:
+            max_enc_length = len(eb)
+
+    for i, eb in enumerate(encoder_batch_list):
+        for _ in range(max_enc_length-len(eb)):
+            encoder_batch_list[i].append(pad_token)
+
+
+    for db in decoder_batch_list:
+        if len(db) > max_dec_length:
+            max_dec_length = len(db)
+
+    for i, db in enumerate(decoder_batch_list):
+        for _ in range(max_dec_length-len(db)):
+            decoder_batch_list[i].append(pad_token)
+
+    encoder_batch_matrix = np.asarray(encoder_batch_list)
+    decoder_batch_matrix = np.asarray(decoder_batch_list)
+
+    # print(word2index['-SOS-'])
+    # print(word2index['-PAD-'])
+    # print(encoder_batch_matrix)
+    # print(decoder_batch_matrix)
+    return encoder_batch_matrix, decoder_batch_matrix
+
+
 
 
 def get_padded_questions(encoder_batch_matrix, word2index):
