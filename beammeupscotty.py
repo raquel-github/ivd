fd85376175ebe928a1b80902bf7652233c1635ee
timbmg <baumgaertner.t@gmail.com>
2017-06-28 20:03:57 +0200

    updated search

--- beammeupscotty.py
+++ beammeupscotty.py
@@ -28,20 +28,20 @@ word2index              = dr.get_word2ind()
 vocab_size              = len(word2index)
 word_embedding_dim      = 512
 hidden_encoder_dim      = 512
-encoder_model_path      = 'Models/bin/enc2017_06_27_17_12_4'
+encoder_model_path      = 'Models/bin/enc2017_06_28_13_28_1 copy'
 
 # Decoder
 hidden_decoder_dim      = 512
 index2word              = dr.get_ind2word()
 visual_features_dim     = 4096
-decoder_model_path      = 'Models/bin/dec2017_06_27_17_12_4'
+decoder_model_path      = 'Models/bin/dec2017_06_28_13_28_1 copy'
 max_length              = dr.get_question_max_length()
 
 # General
 length                  = 11
-topk                    = 1
+topk                    = 3
 batch_size              = 1
-search_mode             = 'beam' # 'beam' or 'sample'
+search_mode             = 'argmax' # 'beam' or 'sample' or 'argmax'
 
 pad_token               = int(word2index['-PAD-'])
 sos_token               = int(word2index['-SOS-'])
@@ -55,6 +55,8 @@ decoder_model.load_state_dict(torch.load(decoder_model_path, map_location=lambda
 
 # get a random game
 gid = randint(0,10000)
+gid = 11
+print("Game:", gid)
 print("URL:", dr.get_image_url(gid))
 print("Mode: ", search_mode)
 #print("Q", dr.get_questions(gid))
@@ -71,71 +73,66 @@ else:
 sos_embedding = encoder_model.get_sos_embedding(use_cuda)
 
 n_questions = 5
-seq_p = [ [0] * n_questions] * topk
-seq_w = [ [''] * n_questions] * topk
+seq_p = []
+seq_w = []
+
 padded_sos = pad_sos(sos_token, pad_token, length, batch_size)
+
 for k in range(topk):
+    #print("+***************************")
+    seq_p.append([])
+    seq_w.append([])
     encoder_model.init_hidden(train_batch=0)
+    #print("Init hidden state")
+
     for n in range(n_questions):
+        #print("##################")
+        seq_p[-1].append(0)
+        seq_w[-1].append('')
+
+        if n == 0:
+            _, encoder_hidden_state = encoder_model(padded_sos, visual_features_batch)
+            #print("Encoding sos")
+        else:
+            enc_in = torch.ones(length+1, 1, out=torch.LongTensor()) * pad_token
+            for i,w in enumerate(seq_w[k][n-1].split()):
+                enc_in[i] = word2index[w]
+
+            _, encoder_hidden_state = encoder_model(enc_in, visual_features_batch)
+            #print("encoding previous q/a")
+
         for l in range(length):
+            #print(k,n,l)
+
+            #print("decoding word")
 
             if l == 0:
-
-                _, encoder_hidden_state     = encoder_model(padded_sos, visual_features_batch)
-                decoder_out                 = decoder_model(visual_features_batch, encoder_hidden_state, sos_embedding)
-
-                if search_mode == 'beam':
-                    wp, w_id                    = decoder_out.topk(topk)
-                    wp, w_id                    = wp.data.numpy()[0][k], int(w_id.data.numpy()[0][k])
-                    seq_p[k][n] += wp
-                    seq_w[k][n] += index2word[str(w_id)] + ' '
-
-                elif search_mode == 'sample':
-                    w_id    = torch.multinomial(torch.exp(decoder_out), 1)
-                    wp      = decoder_out.data[0,w_id.data[0,0]]
-                    seq_p[k][n] += wp
-                    seq_w[k][n] += index2word[str(w_id.data[0,0])] + ' '
-
-
+                decoder_out = decoder_model(visual_features_batch, encoder_hidden_state, sos_embedding)
             else:
-                enc_in = torch.ones(length+1, 1, out=torch.LongTensor()) * pad_token
+                decoder_out = decoder_model(visual_features_batch, encoder_hidden_state)
 
-                enc_in[0,0] = w_id.data[0,0] if search_mode == 'sample' else w_id
+            if search_mode == 'sample':
+                w_id    = torch.multinomial(torch.exp(decoder_out), 1)
+                wp      = decoder_out.data[0,w_id.data[0,0]]
+                seq_p[k][n] += wp
 
-                _, encoder_hidden_state     = encoder_model(enc_in, visual_features_batch)
-                decoder_out                 = decoder_model(visual_features_batch, encoder_hidden_state)
-                if search_mode == 'beam':
-                    wp, w_id                    = decoder_out.topk(topk)
-                    wp, w_id                    = wp.data.numpy()[0][k], int(w_id.data.numpy()[0][k])
-                    seq_p[k][n] += wp
-                    seq_w[k][n] += index2word[str(w_id)] + ' '
+            elif search_mode == 'argmax':
+                wp, w_id = decoder_out.topk(1)
+                seq_p[k][n] += wp.data[0,0]
 
-                    if index2word[str(w_id)] == '-EOS-':
-                        # add answer
-                        if random() <= 0.7:
-                            seq_w[k][n] += 'No'
-                        else:
-                            seq_w[k][n] += 'Yes'
+            #print(index2word[str(w_id.data[0,0])])
+            #print(w_id.data[0,0])
+            seq_w[k][n] += index2word[str(w_id.data[0,0])] + ' '
 
-                        break
-
-                elif search_mode == 'sample':
-                    w_id    = torch.multinomial(torch.exp(decoder_out), 1)
-                    wp      = decoder_out.data[0,w_id.data[0,0]]
-                    seq_p[k][n] += wp
-                    seq_w[k][n] += index2word[str(w_id.data[0,0])] + ' '
-
-                    if index2word[str(w_id.data[0,0])] == '-EOS-':
-                        # add answer
-                        if random() <= 0.7:
-                            seq_w[k][n] += 'No'
-                        else:
-                            seq_w[k][n] += 'Yes'
-
-                        break
-
-
+            if index2word[str(w_id.data[0,0])] == '-EOS-':
+                # add answer
+                if random() <= 0.7:
+                    seq_w[k][n] += 'No'
+                else:
+                    seq_w[k][n] += 'Yes'
+                break
 
 
 print(seq_p)
 print(seq_w)
+print(len(seq_w))
