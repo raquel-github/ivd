568bdc5fbf2636dc74cbdeaed4cad9a272b2857e
aashish <aashish.venkatesh@student.uva.nl>
2017-08-07 12:53:16 +0200

    Oracle Batching w/o comments

---
+++ OpenNMT/ivdModels/Oracle/Oracle.py
@@ -0,0 +1,85 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import transforms, utils
+
+use_cuda = torch.cuda.is_available()
+# use_cuda = False
+
+class Oracle(nn.Module):
+    """docstring for Oracle"""
+    def __init__(self, word_embedding_dim, obj_cat_embedding_dim, hidden_lstm_dim, vocab_size, obj_cat_size):
+        super(Oracle, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.word_embedding_dim = word_embedding_dim
+        self.hidden_lstm_dim = hidden_lstm_dim
+        self.obj_cat_size = obj_cat_size
+        self.obj_cat_embedding_dim = obj_cat_embedding_dim
+
+        # Word embedding Training Model
+        self.word_embeddings = nn.Embedding(self.vocab_size, self.word_embedding_dim)
+        self.obj_cat_embedding = nn.Embedding(self.obj_cat_size, self.obj_cat_embedding_dim)
+
+        # LSTM Model
+        self.lstm = nn.LSTM(self.word_embedding_dim, self.hidden_lstm_dim)
+
+        # Initiliaze the hidden state of the LSTM
+        # self.hidden_lstm = self.init_hidden(self.batch_size)
+
+        self.mlp = nn.Sequential(
+                    nn.Linear(self.hidden_lstm_dim+self.obj_cat_embedding_dim+8, 256),
+                    nn.ReLU(),
+                    nn.Linear(256,256),
+                    nn.ReLU(),
+                    nn.Linear(256,256),
+                    nn.ReLU(),
+                    nn.Dropout(),
+                    nn.Linear(256,3))
+
+    def init_hidden(self, actual_batch_size, split = 'train'):
+        if split == 'train':
+            if use_cuda:
+                return (Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim)).cuda(),
+                        Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim)).cuda())
+            else:
+                return (Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim)),
+                        Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim)))
+        else:
+            if use_cuda:
+                return (Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim), requires_grad=False).cuda(),
+                            Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim), requires_grad=False ).cuda())
+            else:
+                return (Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim), requires_grad=False),
+                            Variable(torch.zeros(1, actual_batch_size, self.hidden_lstm_dim), requires_grad=False ))
+
+
+    def forward(self, split, question_batch, obj_cat_batch, spatial_batch, crop_features, image_features, actual_batch_size):
+        if use_cuda:
+            question_batch = Variable(question_batch).cuda()
+            obj_cat_batch = Variable(obj_cat_batch).cuda()
+            spatial_batch = Variable(spatial_batch).cuda()
+            crop_features = Variable(crop_features, requires_grad=False).cuda()
+            image_features = Variable(image_features, requires_grad=False).cuda()
+        else:
+            question_batch = Variable(question_batch)
+            obj_cat_batch = Variable(obj_cat_batch)
+            spatial_batch = Variable(spatial_batch)
+            crop_features = Variable(crop_features, requires_grad=False)
+            image_features = Variable(image_features, requires_grad=False)
+    
+        question_batch_embedding  = self.word_embeddings(question_batch)
+        # print(question_batch_embedding)
+        obj_cat_batch_embeddding  = self.obj_cat_embedding(obj_cat_batch)
+
+        self.hidden_lstm = self.init_hidden(actual_batch_size, split)
+
+        _, self.hidden_lstm = self.lstm(question_batch_embedding.view(46, actual_batch_size, self.word_embedding_dim), self.hidden_lstm) # 46 == Max length of the question. 
+
+        mlp_in = torch.cat([ image_features, crop_features, spatial_batch, obj_cat_batch_embeddding, self.hidden_lstm[0].squeeze()], 1)
+
+        mlp_out = self.mlp(mlp_in)
+
+        return F.log_softmax(mlp_out)
---
+++ OpenNMT/ivdModels/Oracle/OracleDataset.py
@@ -0,0 +1,60 @@
+import os
+import re
+import json
+import h5py
+import numpy as np
+from collections import defaultdict
+from nltk.tokenize import word_tokenize
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+
+class OracleDataset(Dataset):
+    def __init__(self, split, json_data_file, img_features_file, img2id_file, crop_features_file, crop2id_file, vocab_json_file):
+        """
+        split: ['train', 'val', 'test']
+        """
+        with open(json_data_file) as file:
+            self.questions = json.load(file)['questions']
+        with open(img2id_file) as file:
+            self.img2id = json.load(file)[split+'2id']
+        with open(crop2id_file) as file:
+            self.crop2id = json.load(file)[split+'crops2id']
+        
+        img_h5data = h5py.File(img_features_file, 'r')
+        self.img_features = img_h5data[split+'_img_features']
+        del img_h5data
+        crop_h5data = h5py.File(crop_features_file, 'r')
+        self.crop_features = crop_h5data[split+'_crop_features']
+        del crop_h5data
+        
+        self.ans2id = {'no':0, 'yes':1, 'n/a':2}
+        with open(vocab_json_file) as file:
+            self.word2ind = json.load(file)['word2ind']
+            
+    def __len__(self):
+        return len(self.questions[:100])
+    
+    def __getitem__(self, idx):
+        crop_features = self.crop_features[self.crop2id[self.questions[idx]['crop_features']]]
+        img_features = self.img_features[self.img2id[self.questions[idx]['img_features']]]
+        spaital = torch.FloatTensor(self.questions[idx]['spatial'])
+        obj_cat = self.questions[idx]['obj_cat']
+        
+        raw_question = self.questions[idx]['question']
+        question = (np.ones(15,'uint8')*self.word2ind['-PAD-']).tolist()
+        question_words = re.findall(r'\w+', raw_question.lower())+['?']
+        for wid, word in enumerate(question_words):
+            if word in self.word2ind:
+                question[wid] = self.word2ind[word]
+            else:
+                question[wid] = self.word2ind['-UNK-']
+            
+        question = torch.LongTensor(question)
+        answer = self.ans2id[self.questions[idx]['answer'].lower()]
+        
+        sample = {'question':question, 'answer': answer, 'crop_features':crop_features, 'img_features':img_features,\
+                  'spaital':spaital, 'obj_cat':obj_cat}
+        
+        return sample
