203d40962b1ba6fc1cacd82283ce07a21ec283a6
aashish <aashish.venkatesh@student.uva.nl>
2017-09-09 10:57:58 +0200

    ResNet done

--- seq2seq/Preprocessing/ExtractImgFeaturesV2.py
+++ seq2seq/Preprocessing/ExtractImgFeaturesV2.py
@@ -6,17 +6,22 @@ import torch
 import numpy as np
 from time import time
 
+from ResNet_Feature_Extract import ResNet_Feature_Extract
 from VGG_Feature_Extract import VGG_Feature_Extract
 
-def extract_img_features(dir_path, img_list, vgg_ext, use_cuda):
-	img_features = np.zeros((len(img_list), 4096))
+def extract_img_features(dir_path, img_list, model, selected_model,use_cuda):
+	if selected_model == 'VGG':
+		img_features = np.zeros((len(img_list), 4096))
+	else:
+		img_features = np.zeros((len(img_list), 2048))
+
 	name2id = {}
 
 	for idx, img in enumerate(img_list):
 		img_path = dir_path+img
 		name2id[img] = idx
 
-		features = vgg_ext.get_features(img_path)
+		features = model.get_features(img_path)
 		if use_cuda:
 			features = features.cpu()
 		img_features[idx] = features.data.numpy()
@@ -25,6 +30,8 @@ def extract_img_features(dir_path, img_list, vgg_ext, use_cuda):
  
 
 start = time()
+
+selected_model = 'ResNet'
 
 use_cuda = torch.cuda.is_available()
 
@@ -36,20 +43,25 @@ train_crops = '/home/aashish/Documents/ProjectAI/data/GuessWhat/Train_Crops/'
 val_crops   = '/home/aashish/Documents/ProjectAI/data/GuessWhat/Validation_Crops/'
 test_crops  = '/home/aashish/Documents/ProjectAI/data/GuessWhat/Test_Crops/'
 
-vgg_ext = VGG_Feature_Extract()
+if selected_model == 'VGG':
+	model = VGG_Feature_Extract()
+elif selected_model == 'ResNet':
+	model = ResNet_Feature_Extract()
+else:
+	raise Exception('Wrong Model Name!!')
 
-train_img_features, train2id = extract_img_features(train_img, os.listdir(train_img), vgg_ext, use_cuda) 
-val_img_features, val2id = extract_img_features(val_img, os.listdir(val_img), vgg_ext, use_cuda)
-test_img_features, test2id = extract_img_features(test_img, os.listdir(test_img), vgg_ext, use_cuda)
+train_img_features, train2id = extract_img_features(train_img, os.listdir(train_img), model, selected_model, use_cuda) 
+val_img_features, val2id = extract_img_features(val_img, os.listdir(val_img), model, selected_model, use_cuda)
+test_img_features, test2id = extract_img_features(test_img, os.listdir(test_img), model, selected_model, use_cuda)
 
-img_file = h5py.File('image_features.h5', 'w')
+img_file = h5py.File(selected_model+'image_features.h5', 'w')
 img_file.create_dataset('train_img_features', dtype='float32', data=train_img_features)
 img_file.create_dataset('val_img_features', dtype='float32', data=val_img_features)
 img_file.create_dataset('test_img_features', dtype='float32', data=test_img_features)
 img_file.close()
 
 json_data = {'train2id':train2id, 'val2id':val2id, 'test2id':test2id}
-with open('img_features2id.json', 'a') as file:
+with open(selected_model+'img_features2id.json', 'a') as file:
 	json.dump(json_data, file)
 
 print('Image Features extracted. Doing the crops now')
@@ -59,18 +71,18 @@ del train_img_features, val_img_features, test_img_features, train2id, val2id, t
 start = time()
 print('Begin with crops')
 
-train_crop_features, traincrops2id = extract_img_features(train_crops, os.listdir(train_crops), vgg_ext, use_cuda)
-val_crop_features, valcrops2id = extract_img_features(val_crops, os.listdir(val_crops), vgg_ext, use_cuda)
-test_crop_features, testcrops2id = extract_img_features(test_crops, os.listdir(test_crops), vgg_ext, use_cuda)
+train_crop_features, traincrops2id = extract_img_features(train_crops, os.listdir(train_crops), model, selected_model, use_cuda)
+val_crop_features, valcrops2id = extract_img_features(val_crops, os.listdir(val_crops), model, selected_model, use_cuda)
+test_crop_features, testcrops2id = extract_img_features(test_crops, os.listdir(test_crops), model, selected_model, use_cuda)
 
-crop_file = h5py.File('crop_features.h5', 'w')
+crop_file = h5py.File(selected_model+'crop_features.h5', 'w')
 crop_file.create_dataset('train_crop_features', dtype='float32', data=train_crop_features)
 crop_file.create_dataset('val_crop_features', dtype='float32', data=val_crop_features)
 crop_file.create_dataset('test_crop_features', dtype='float32', data=test_crop_features)
 crop_file.close()
 
 json_data = {'traincrops2id':traincrops2id, 'valcrops2id':valcrops2id, 'testcrops2id':testcrops2id} 
-with open('crop_features2id.json', 'a') as file:
+with open(selected_model+'crop_features2id.json', 'a') as file:
 	json.dump(json_data, file)
 
 print('Crop features extracted')
--- seq2seq/Preprocessing/ResNet_Feature_Extract.py
+++ seq2seq/Preprocessing/ResNet_Feature_Extract.py
@@ -1,44 +1,34 @@
-import vgg
-import io
-import requests
-from PIL import Image
 import torch
+import torch.nn as nn
 from torchvision import models, transforms
 from torch.autograd import Variable
-import torch.nn as nn
-import numpy
-import os
+from torchvision.models import resnet
+
+import numpy as np
+from PIL import Image
 
 use_cuda = torch.cuda.is_available()
 
-class VGG_Feature_Extract():
-
+class ResNet_Feature_Extract():
+    """docstring for ResNet_Feature_Extract"""
     def __init__(self):
-        # load preptrained model
-        self.model = vgg.vgg16_bn(pretrained=True)
+        super(ResNet_Feature_Extract, self).__init__()
 
+        self.model = resnet.resnet152(pretrained=True)
 
-        # replace fullyconnceted layer to get onyl features (and not classification)
-        self.model.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(4096, 4096)
-        )
+        self.model.fc = nn.Linear(512 * 4, 512 * 4)
 
         # Using the imagenet mean and std
         mean =  [0.485, 0.456, 0.406]
         std  =  [0.229, 0.224, 0.225]
- 
+
         # normaliztion pipeline: (img-mean) / std
         self.normalize = transforms.Normalize(
             mean=mean,
             std=std
         )
- 
- 
-        # preprocessing pipeline
 
+        # preprocessing pipeline
         self.preprocess = transforms.Compose([
             transforms.Scale(224),
             transforms.CenterCrop(224),
@@ -47,19 +37,16 @@ class VGG_Feature_Extract():
          ])
 
         if use_cuda:
-            self.model.cuda();
-
-
+            self.model.cuda()
 
     def get_features(self, img_p):
         """ Given an image path, this function will return the VGG features """
         img = Image.open(img_p)
-        if len(numpy.array(img).shape) == 2:
+        if len(np.array(img).shape) == 2:
             rgb = Image.new('RGB',img.size)
             rgb.paste(img)
             img = rgb
-            # print(img_p)
-            # print(numpy.array(img).shape)
+            
         img_tensor = self.preprocess(img)
         img_tensor.unsqueeze_(0)
         if use_cuda:
@@ -68,15 +55,3 @@ class VGG_Feature_Extract():
             img_variable = Variable(img_tensor)
         # print(img_p)
         return self.model(img_variable)
-
-
-# Example How To Extract Features
-# images_path = str('../data/Sample/MS_COCO')
-
-
-# fe = VGG_Feature_Extract(images_path)
-
-# for f in os.listdir(images_path+"/"):
-#             if f.endswith(".jpg"):
-#                 img_p = images_path + "/" + f
-#                 print(fe.get_features(img_p))
